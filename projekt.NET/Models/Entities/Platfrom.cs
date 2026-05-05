2b6dae1c8570bace8d3566a0097248f952d25e54
Michał <92009@student.pb.edu.pl>
2026-05-05 23:45:15 +0200

    nugety, encje, appdbcontext

---
+++ projekt.NET/Models/Entities/Platfrom.cs
@@ -0,0 +1,11 @@
+﻿namespace projekt.NET.Models.Entities
+{
+    //relacja wiele do wielu - jedna gra może być na wielu platformach, a jedna platforma może mieć wiele gier
+    public class Platfrom
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        //lista gier na tej platformie 
+        public ICollection<Game> Games { get; set; } = new List<Game>();
+    }
+}
